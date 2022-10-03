9e3be6b5d160c48c181d35893e3e7cd5698f8c04
Henry Schreiner <henryschreineriii@gmail.com>
2022-10-02 23:30:11 -0400

    fix: Any -> object were possible
    Signed-off-by: Henry Schreiner <henryschreineriii@gmail.com>

--- src/scikit_build_core/_logging.py
+++ src/scikit_build_core/_logging.py
@@ -1,7 +1,6 @@
 from __future__ import annotations
 
 import logging
-from typing import Any
 
 __all__ = ["logger", "raw_logger", "ScikitBuildLogger"]
 
@@ -19,7 +18,7 @@ raw_logger.setLevel(logging.DEBUG)  # TODO: configure
 class FStringMessage:
     "This class captures a formatted string message and only produces it on demand."
 
-    def __init__(self, fmt: str, *args: Any, **kwargs: Any) -> None:
+    def __init__(self, fmt: str, *args: object, **kwargs: object) -> None:
         self.fmt = fmt
         self.args = args
         self.kwargs = kwargs
@@ -33,25 +32,25 @@ class ScikitBuildLogger:
     def __init__(self, logger: logging.Logger) -> None:
         self.logger = logger
 
-    def debug(self, msg: str, *args: Any, **kwargs: Any) -> None:
+    def debug(self, msg: str, *args: object, **kwargs: object) -> None:
         self.logger.debug(FStringMessage(msg, *args, **kwargs))
 
-    def info(self, msg: str, *args: Any, **kwargs: Any) -> None:
+    def info(self, msg: str, *args: object, **kwargs: object) -> None:
         self.logger.info(FStringMessage(msg, *args, **kwargs))
 
-    def warning(self, msg: str, *args: Any, **kwargs: Any) -> None:
+    def warning(self, msg: str, *args: object, **kwargs: object) -> None:
         self.logger.warning(FStringMessage(msg, *args, **kwargs))
 
-    def error(self, msg: str, *args: Any, **kwargs: Any) -> None:
+    def error(self, msg: str, *args: object, **kwargs: object) -> None:
         self.logger.error(FStringMessage(msg, *args, **kwargs))
 
-    def critical(self, msg: str, *args: Any, **kwargs: Any) -> None:
+    def critical(self, msg: str, *args: object, **kwargs: object) -> None:
         self.logger.critical(FStringMessage(msg, *args, **kwargs))
 
-    def exception(self, msg: str, *args: Any, **kwargs: Any) -> None:
+    def exception(self, msg: str, *args: object, **kwargs: object) -> None:
         self.logger.exception(FStringMessage(msg, *args, **kwargs))
 
-    def log(self, level: int, msg: str, *args: Any, **kwargs: Any) -> None:
+    def log(self, level: int, msg: str, *args: object, **kwargs: object) -> None:
         self.logger.log(level, FStringMessage(msg, *args, **kwargs))
 
     def setLevel(self, level: int) -> None:
