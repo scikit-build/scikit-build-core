ace7bbcd8a15bf3fd17c56f159b9b6aff24a539c
Henry Schreiner <henryschreineriii@gmail.com>
2022-10-29 00:19:52 -0400

    tests: add test for simplist C-only like in README
    Signed-off-by: Henry Schreiner <henryschreineriii@gmail.com>

---
+++ tests/test_simplest_c.py
@@ -0,0 +1,27 @@
+from pathlib import Path
+
+import pytest
+
+from scikit_build_core.build import build_wheel
+
+DIR = Path(__file__).parent.resolve()
+SIMPLEST = DIR / "packages/simplest_c"
+
+
+@pytest.mark.compile
+@pytest.mark.configure
+def test_pep517_wheel(tmp_path, monkeypatch, virtualenv):
+    dist = tmp_path / "dist"
+    dist.mkdir()
+    monkeypatch.chdir(SIMPLEST)
+    out = build_wheel(str(dist))
+    (wheel,) = dist.glob("scikit_build_simplest-0.0.1-*.whl")
+    assert wheel == dist / out
+
+    virtualenv.run(f"python -m pip install {wheel}")
+
+    version = virtualenv.run(
+        'python -c "from scikit_build_simplest import square; print(square(2))"',
+        capture=True,
+    )
+    assert version.strip() == "4.0"
