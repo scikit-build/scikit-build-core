e8456558da8e680aef0c5a4d46cc78c7e556ca95
Henry Schreiner <HenrySchreinerIII@gmail.com>
2022-11-07 10:32:40 -0500

    feat: use setup keyword support (#42)
    Signed-off-by: Henry Schreiner <henryschreineriii@gmail.com>
    
    Signed-off-by: Henry Schreiner <henryschreineriii@gmail.com>

--- src/scikit_build_core/_compat/typing.py
+++ src/scikit_build_core/_compat/typing.py
@@ -3,11 +3,11 @@ from __future__ import annotations
 import sys
 
 if sys.version_info < (3, 8):
-    from typing_extensions import Protocol, runtime_checkable
+    from typing_extensions import Literal, Protocol, runtime_checkable
 else:
-    from typing import Protocol, runtime_checkable
+    from typing import Literal, Protocol, runtime_checkable
 
-__all__ = ["Protocol", "runtime_checkable"]
+__all__ = ["Protocol", "runtime_checkable", "Literal"]
 
 
 def __dir__() -> list[str]:
