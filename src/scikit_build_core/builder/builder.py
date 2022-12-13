c18f38c8f06bb0ecfa249385e79fadd89ab6916c
Henry Schreiner <HenrySchreinerIII@gmail.com>
2022-12-13 15:39:55 -0800

    fix: EXT_SUFFIX wrong before 3.8.7 (#160)
    This was fixed in CPython 3.8.7.
    
    Signed-off-by: Henry Schreiner <henryschreineriii@gmail.com>
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- src/scikit_build_core/builder/builder.py
+++ src/scikit_build_core/builder/builder.py
@@ -120,8 +120,14 @@ class Builder:
             # Workaround for bug in PyPy and packaging that is not handled in CMake
             # According to PEP 3149, SOABI and EXT_SUFFIX are interchangeable (and
             # the latter is much more likely to be correct as it is used elsewhere)
-            ext_suffix = sysconfig.get_config_var("EXT_SUFFIX")
-            assert ext_suffix
+            if sys.version_info < (3, 8, 7):
+                # See https://github.com/python/cpython/issues/84006
+                import distutils.sysconfig  # pylint: disable=deprecated-module
+
+                ext_suffix = distutils.sysconfig.get_config_var("EXT_SUFFIX")
+            else:
+                ext_suffix = sysconfig.get_config_var("EXT_SUFFIX")
+            assert isinstance(ext_suffix, str)
             cache_config["SKBUILD_SOABI"] = ext_suffix.rsplit(".", 1)[0].lstrip(".")
 
         if cache_entries:
