3af19108b1064a378bc8cfaa9b57b6d9de1a71bf
Henry Schreiner <HenrySchreinerIII@gmail.com>
2023-04-12 18:24:58 -0400

    Update test_custom_modules.py

--- tests/test_custom_modules.py
+++ tests/test_custom_modules.py
@@ -9,7 +9,5 @@ PROJECT_DIR = DIR / "packages" / "custom_cmake"
 def test_ep(isolated):
     isolated.install("hatchling", "scikit-build-core[pyproject]")
     isolated.install(PROJECT_DIR / "extern", isolated=False)
-    isolated.install(
-        PROJECT_DIR, "-v", "--config-settings=logging.level=DEBUG", isolated=False
-    )
+    isolated.install(PROJECT_DIR, "-v", isolated=False)
     # Needs script fix: assert isolated.run("script1") == ""
