e10f4ea929aec5981e3d854d204f73f19d14c886
Henry Schreiner <henryschreineriii@gmail.com>
2023-05-16 19:51:11 -0400

    tests: update for closer match to setuptools
    Signed-off-by: Henry Schreiner <henryschreineriii@gmail.com>

--- tests/packages/simple_setuptools_ext/setup.py
+++ tests/packages/simple_setuptools_ext/setup.py
@@ -1,10 +1,12 @@
-from setuptools import setup
+from setuptools import find_packages, setup
 
 setup(
     name="cmake-example",
     version="0.0.1",
     cmake_source_dir=".",
     zip_safe=False,
+    package_dir={"": "src"},
+    packages=find_packages(),
     extras_require={"test": ["pytest>=6.0"]},
     python_requires=">=3.7",
 )
