a99bc34bdb9be1cb70bd930a93ee7c8e3437bcdb
Henry Schreiner <henryschreineriii@gmail.com>
2023-06-09 18:00:43 -0400

    tests: add testing for scripts processing
    Signed-off-by: Henry Schreiner <henryschreineriii@gmail.com>

--- tests/test_custom_modules.py
+++ tests/test_custom_modules.py
@@ -10,4 +10,7 @@ def test_ep(isolated):
     isolated.install("hatchling", "scikit-build-core[pyproject]")
     isolated.install(PROJECT_DIR / "extern", isolated=False)
     isolated.install(PROJECT_DIR, "-v", isolated=False)
-    # Needs script fix: assert isolated.run("script1") == ""
+    script = isolated.run("script1", capture=True).strip()
+    pysys = isolated.execute("import sys; print(sys.executable)").strip()
+    contents = Path(script).read_text()
+    assert contents.startswith(f"#!{pysys}")
