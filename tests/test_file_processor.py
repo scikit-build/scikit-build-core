80bb7f9a8aa71d690c38442f03b3802b96e7805a
Henry Schreiner <henryschreineriii@gmail.com>
2023-06-10 10:31:20 -0400

    tests: skipifs for symlinks
    Signed-off-by: Henry Schreiner <henryschreineriii@gmail.com>

--- tests/test_file_processor.py
+++ tests/test_file_processor.py
@@ -8,6 +8,14 @@ import pytest
 from scikit_build_core.build._file_processor import each_unignored_file
 
 
+@pytest.mark.skipif(
+    sys.version_info < (3, 8) and sys.platform.startswith("win"),
+    reason="Python 3.8+ required for symlinks on Windows",
+)
+@pytest.mark.skipif(
+    sys.implementation.name == "pypy" and sys.platform.startswith("win"),
+    reason="PyPy on Windows does not support symlinks",
+)
 def test_on_each_with_symlink(tmp_path: Path, monkeypatch: pytest.MonkeyPatch) -> None:
     """
     Test that each_unignored_file() does not follow symlinks.
@@ -30,15 +38,16 @@ def test_on_each_with_symlink(tmp_path: Path, monkeypatch: pytest.MonkeyPatch) -
     exposed_symlink = dir / "exposed_symlink"
     exposed_symlink.symlink_to("../hidden_dir")
 
-    has_symlinks = (
-        not sys.platform.startswith("win")
-        or exposed_symlink.joinpath("file2").is_file()
-    )
+    if (
+        sys.platform.startswith("win")
+        and not exposed_symlink.joinpath("file2").is_file()
+    ):
+        pytest.skip("Windows symlink support not available")
 
     # Test that each_unignored_file() follows the symlink
     assert sorted(each_unignored_file(Path("."))) == [
         gitignore,
-        exposed_symlink / "file2" if has_symlinks else exposed_symlink,
+        exposed_symlink / "file2",
         file1,
         file2,
     ]
