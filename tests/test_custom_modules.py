7f3e34675d7c20dd2b7300dde0d7e845d1c92365
Henry Schreiner <henryschreineriii@gmail.com>
2023-06-10 10:57:22 -0400

    tests: skip rest of test on Windows
    Signed-off-by: Henry Schreiner <henryschreineriii@gmail.com>

--- tests/test_custom_modules.py
+++ tests/test_custom_modules.py
@@ -1,6 +1,9 @@
 from __future__ import annotations
 
+import sys
 from pathlib import Path
+
+import pytest
 
 DIR = Path(__file__).parent
 PROJECT_DIR = DIR / "packages" / "custom_cmake"
@@ -10,6 +13,11 @@ def test_ep(isolated):
     isolated.install("hatchling", "scikit-build-core[pyproject]")
     isolated.install(PROJECT_DIR / "extern", isolated=False)
     isolated.install(PROJECT_DIR, "-v", isolated=False)
+
+    if sys.platform.startswith("win"):
+        # TODO: maybe figure out how to make this work on windows?
+        pytest.skip("Can't run script on Windows")
+
     script = isolated.run("script1", capture=True).strip()
     pysys = isolated.execute("import sys; print(sys.executable)").strip()
     contents = Path(script).read_text()
