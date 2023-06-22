287d1e7bc8c6abc169ad04f41fafbbc616ff178b
Henry Schreiner <henryschreineriii@gmail.com>
2023-06-22 16:20:09 -0400

    tests: get packages of interest from pyproject.toml
    Signed-off-by: Henry Schreiner <henryschreineriii@gmail.com>

--- tests/conftest.py
+++ tests/conftest.py
@@ -21,7 +21,14 @@ else:
     from importlib import metadata
     from typing import Literal, overload
 
+if sys.version_info < (3, 11):
+    import tomli as tomllib
+else:
+    import tomllib
+
+
 import pytest
+from packaging.requirements import Requirement
 
 DIR = Path(__file__).parent.resolve()
 BASE = DIR.parent
@@ -301,20 +308,19 @@ def pytest_collection_modifyitems(items: list[pytest.Item]) -> None:
 
 
 def pytest_report_header() -> str:
-    interesting_packages = [
-        "build",
-        "packaging",
-        "pathspec",
-        "pip",
-        "pybind11",
-        "pyproject_metadata",
-        "scikit_build_core",
-        "setuptools",
-        "virtualenv",
-        "wheel",
-    ]
+    with BASE.joinpath("pyproject.toml").open("rb") as f:
+        pyproject = tomllib.load(f)
+    project = pyproject.get("project", {})
+
+    pkgs = project.get("dependencies", [])
+    pkgs += [p for ps in project.get("optional-dependencies", {}).values() for p in ps]
+    if "name" in project:
+        pkgs.append(project["name"])
+    interesting_packages = {Requirement(p).name for p in pkgs}
+    interesting_packages.add("pip")
+
     valid = []
-    for package in interesting_packages:
+    for package in sorted(interesting_packages):
         with contextlib.suppress(ModuleNotFoundError):
             valid.append(f"{package}=={metadata.version(package)}")
     reqs = " ".join(valid)
