63cd1f2ae2ad34f30e13c4c3250a230d368e63f8
Henry Schreiner <HenrySchreinerIII@gmail.com>
2023-06-23 15:59:32 -0400

    Apply suggestions from code review

--- tests/test_logging.py
+++ tests/test_logging.py
@@ -3,7 +3,7 @@ from scikit_build_core._logging import rich_print
 
 def test_rich_print_nocolor(capsys, monkeypatch):
     monkeypatch.setenv("NO_COLOR", "1")
-    monkeypatch.delenv("FORCE_COLOR")
+    monkeypatch.delenv("FORCE_COLOR", raising=False)
     rich_print("[red]hello[/red] world", end="")
     assert capsys.readouterr().out == "hello world"
 
@@ -17,6 +17,6 @@ def test_rich_print_nocolor_forcecolor(capsys, monkeypatch):
 
 def test_rich_print_forcecolor(capsys, monkeypatch):
     monkeypatch.setenv("FORCE_COLOR", "1")
-    monkeypatch.delenv("NO_COLOR")
+    monkeypatch.delenv("NO_COLOR", raising=False)
     rich_print("[red bold]hello[/bold] world", end="")
     assert capsys.readouterr().out == "\33[91m\33[1mhello\33[22m world\33[0m"
