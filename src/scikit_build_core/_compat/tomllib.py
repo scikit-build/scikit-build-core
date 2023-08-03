52ad4df831490941b8dbbd499833479b9459fb70
Henry Schreiner <HenrySchreinerIII@gmail.com>
2023-08-03 12:56:18 -0400

    tests: validate schemas in regular testing (#450)
    Followup to https://github.com/scikit-build/scikit-build-core/pull/447
    based on testing suggestion from @abravalheri.
    
    Signed-off-by: Henry Schreiner <henryschreineriii@gmail.com>

--- src/scikit_build_core/_compat/tomllib.py
+++ src/scikit_build_core/_compat/tomllib.py
@@ -3,11 +3,11 @@ from __future__ import annotations
 import sys
 
 if sys.version_info < (3, 11):
-    from tomli import load
+    from tomli import load, loads
 else:
-    from tomllib import load
+    from tomllib import load, loads
 
-__all__ = ["load"]
+__all__ = ["load", "loads"]
 
 
 def __dir__() -> list[str]:
