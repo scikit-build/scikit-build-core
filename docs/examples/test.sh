0cd69e1af2c535769381a5680c929d452d791395
Cristian Le <cristian.le@mpsd.mpg.de>
2023-08-14 17:18:24 +0200

    ci: Fix Fedora CI (#464)
    - Use fedora packages with `--no-index` and `--no-build-isolation`
    - Enabled missing tests
    - Placeholder for nanobind tests (Not yet packaged on fedora)
    
    ---------
    
    Signed-off-by: Cristian Le <cristian.le@mpsd.mpg.de>

--- docs/examples/test.sh
+++ docs/examples/test.sh
@@ -6,16 +6,27 @@ source /usr/share/beakerlib/beakerlib.sh || exit 1
 rlJournalStart
     rlPhaseStartSetup
         rlRun "tmp=\$(mktemp -d)" 0 "Create tmp directory"
-		    rlRun "rsync -r ${TMT_SOURCE_DIR:-$TMT_TREE}$TMT_TEST_NAME/ $tmp" 0 "Copy example project"
-		    rlRun "rsync -r ${TMT_SOURCE_DIR:-$TMT_TREE}$TMT_TEST_NAME/../test.py $tmp" 0 "Copy test.py file"
+        if [ -z ${TMT_SOURCE_DIR} ]; then
+          tmt_root=${TMT_TREE}
+        else
+          tmt_root=${TMT_SOURCE_DIR}/scikit_build_core-*
+        fi
+		    rlRun "rsync -r ${tmt_root}$TMT_TEST_NAME/ $tmp" 0 "Copy example project"
+        if [ "${HAS_PYTEST}" != True ]; then
+		      rlRun "rsync -r ${tmt_root}/docs/examples/getting_started/test.py $tmp" 0 "Copy test.py file"
+        fi
         rlRun "pushd $tmp"
         rlRun "tree" 0 "Show directory tree"
         rlRun "set -o pipefail"
     rlPhaseEnd
 
     rlPhaseStartTest
-        rlRun "pip install . --config-settings=cmake.verbose=true" 0 "Build the python project"
-        rlRun "python3 test.py" 0 "Test project is installed correctly"
+        rlRun "pip install --user . --config-settings=cmake.verbose=true --no-index --no-build-isolation" 0 "Build the python project"
+        if [ "${HAS_PYTEST}" == True ]; then
+          rlRun "pytest" 0 "Run built-in pytest"
+        else
+          rlRun "python3 test.py" 0 "Test project is installed correctly"
+        fi
     rlPhaseEnd
 
     rlPhaseStartCleanup
