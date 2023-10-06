c05d57011dd801a5a2562c02cf84d98d2b9b2c87
Henry Schreiner <henryschreineriii@gmail.com>
2023-10-06 18:52:41 -0400

    fix(schema): extra validation for metadata
    Signed-off-by: Henry Schreiner <henryschreineriii@gmail.com>

--- src/scikit_build_core/settings/skbuild_schema.py
+++ src/scikit_build_core/settings/skbuild_schema.py
@@ -13,6 +13,25 @@ def __dir__() -> list[str]:
     return __all__
 
 
+METADATA = [
+    "version",
+    "description",
+    "license",
+    "readme",
+    "requires-python",
+    "dependencies",
+    "optional-dependencies",
+    "entrypoints",
+    "authors",
+    "maintainers",
+    "urls",
+    "classifiers",
+    "keywords",
+    "scripts",
+    "gui-scripts",
+]
+
+
 def generate_skbuild_schema(tool_name: str = "scikit-build") -> dict[str, Any]:
     "Generate the complete schema for scikit-build settings."
     assert tool_name == "scikit-build", "Only scikit-build is supported."
@@ -21,7 +40,7 @@ def generate_skbuild_schema(tool_name: str = "scikit-build") -> dict[str, Any]:
     from .skbuild_model import ScikitBuildSettings
 
     schema = {
-        "$schema": "http://json-schema.org/draft-07/schema#",
+        "$schema": "http://json-schema.org/draft-07/schema",
         "$id": "https://github.com/scikit-build/scikit-build-core/blob/main/src/scikit_build_core/resources/scikit-build.schema.json",
         "description": "Scikit-build-core's settings.",
         **to_json_schema(ScikitBuildSettings, normalize_keys=True),
@@ -45,6 +64,22 @@ def generate_skbuild_schema(tool_name: str = "scikit-build") -> dict[str, Any]:
 
     schema["properties"]["generate"]["items"] = {
         "oneOf": [generate_tmpl, generate_path]
+    }
+
+    schema["$defs"] = {
+        "metadata": {
+            "type": "object",
+            "properties": {
+                "provider": {"type": "string"},
+                "provider-path": {"type": "string"},
+            },
+        }
+    }
+
+    del schema["properties"]["metadata"]["patternProperties"]
+    schema["properties"]["metadata"]["additionalProperties"] = False
+    schema["properties"]["metadata"]["properties"] = {
+        m: {"$ref": "#/$defs/metadata"} for m in METADATA
     }
 
     props = {k: {"$ref": f"#/properties/{k}"} for k in schema["properties"]}
