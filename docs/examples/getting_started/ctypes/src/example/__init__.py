4697f07c72b04232f98fc4f40fdec4d201174977
Henry Schreiner <henryschreineriii@gmail.com>
2023-10-20 10:26:42 -0400

    docs: remove example incorrectly added
    Signed-off-by: Henry Schreiner <henryschreineriii@gmail.com>

--- docs/examples/getting_started/ctypes/src/example/__init__.py
+++
@@ -1,7 +0,0 @@
-# libexample = ctypes.CDLL(files("example") / "libexample.so")
-
-# square = libexample.square
-# square.argtypes = [ctypes.c_float]
-# square.restype = ctypes.c_float
-
-# __all__ = ["square"]
