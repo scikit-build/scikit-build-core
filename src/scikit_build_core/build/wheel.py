cfbc797093670f633f3ec72ee8ac2c8783e186be
Henry Schreiner <henryschreineriii@gmail.com>
2024-01-02 18:07:30 -0500

    fix: include license file entry in search
    Signed-off-by: Henry Schreiner <henryschreineriii@gmail.com>

--- src/scikit_build_core/build/wheel.py
+++ src/scikit_build_core/build/wheel.py
@@ -213,10 +213,13 @@ def _build_wheel_impl(
         else:
             install_dir = wheel_dirs[targetlib] / settings.wheel.install_dir
 
+        # Include the metadata license.file entry if provided
+        license_file_globs = list(settings.wheel.license_files)
+        if metadata.license and metadata.license.file:
+            license_file_globs.append(str(metadata.license.file))
+
         license_files = {
-            x: x.read_bytes()
-            for y in settings.wheel.license_files
-            for x in Path().glob(y)
+            x: x.read_bytes() for y in license_file_globs for x in Path().glob(y)
         }
         if settings.wheel.license_files and not license_files:
             logger.warning(
