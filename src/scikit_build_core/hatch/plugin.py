2db26ea1c9435c12aecfc9eff51a04a04701eb47
Aryaman Jeendgar <jeendgararyaman@gmail.com>
2024-03-15 14:25:15 +0100

    Mimicking `wheel.py`, cleanup, WiP

--- src/scikit_build_core/hatch/plugin.py
+++ src/scikit_build_core/hatch/plugin.py
@@ -9,9 +9,12 @@ from typing import Any
 from hatchling.builders.hooks.plugin.interface import BuildHookInterface
 
 from .._compat import tomllib
-from ..builder.builder import Builder
 from ..cmake import CMake, CMaker
+from ..builder.builder import Builder, archs_to_tags, get_archs
+from ..builder.wheel_tag import WheelTag
+from ..build._init import setup_logging
 from ..settings.skbuild_read_settings import SettingsReader
+# from ..settings.metadata import get_standard_metadata
 
 __all__ = ["ScikitBuildHook"]
 
@@ -24,10 +27,6 @@ class ScikitBuildHook(BuildHookInterface):  # type: ignore[type-arg]
     PLUGIN_NAME = "scikit-build"
 
     def initialize(self, version: str, build_data: dict[str, Any]) -> None:  # noqa: ARG002
-        if self.target_name == "sdist":
-            build_data["artifacts"].append("CMakeLists.txt")  # Needs full list, etc.
-            return
-
         pyproject_path = Path("pyproject.toml")
         with pyproject_path.open("rb") as ft:
             pyproject = tomllib.load(ft)
@@ -40,6 +39,7 @@ class ScikitBuildHook(BuildHookInterface):  # type: ignore[type-arg]
             pyproject, {}, state=self.target_name, extra_settings=config_dict
         )
         settings = settings_reader.settings
+        setup_logging(settings.logging.level)
 
         if not settings.wheel.cmake:
             msg = "CMake is required for scikit-build"
@@ -49,17 +49,54 @@ class ScikitBuildHook(BuildHookInterface):  # type: ignore[type-arg]
 
         settings_reader.validate_may_exit()
 
+        # metadata = get_standard_metadata(pyproject, settings)
+
+        if self.target_name == "sdist":
+            if settings.sdist.include or settings.sdist.exclude or settings.sdist.reproducible:
+                msg = ""
+                raise ValueError(msg)
+
+            if settings.sdist.cmake:
+                msg = "Not currently supported for SDist builds"
+                raise ValueError(msg)
+
+            build_data["artifacts"].append("CMakeLists.txt")  # Needs full list, etc.
+            return
+
+        tags = WheelTag.compute_best(
+            archs_to_tags(get_archs(os.environ)),
+            settings.wheel.py_api,
+            expand_macos=settings.wheel.expand_macos_universal_tags,
+            build_tag=settings.wheel.build_tag,
+        )
+        build_data['tag'] = str(tags)
+
+        # if settings.wheel.packages or settings.wheel.license_files:
+        #     msg = ""
+        #     raise ValueError(msg)
+
+        # if settings.wheel.cmake:
+        #     msg = "Not currently supported for wheel builds"
+        #     raise ValueError(msg)
+
         source_dir = Path.cwd()
         if settings.cmake.source_dir:
-            print(f"source-dir: {settings.cmake.source_dir}")
             source_dir = Path.cwd() / settings.cmake.source_dir
 
         build_dir = Path(TemporaryDirectory().name)
         if settings.build_dir:
-            print(f"build-dir: {settings.build_dir}")
             build_dir = Path.cwd() / settings.build_dir
 
-        prefix = Path.cwd() / "prefix"
+        if settings.wheel.platlib is None:
+            targetlib = "platlib" if settings.wheel.cmake else "purelib"
+        else:
+            targetlib = "platlib" if settings.wheel.platlib else "purelib"
+
+        # prefix = Path.cwd() / "prefix"
+        if settings.wheel.install_dir:
+            prefix = Path.cwd() / targetlib / settings.wheel.install_dir
+        else:
+            prefix = Path.cwd() / targetlib
 
         config = CMaker(
             cmake,
@@ -69,15 +106,26 @@ class ScikitBuildHook(BuildHookInterface):  # type: ignore[type-arg]
         )
 
         builder = Builder(settings, config)
-        builder.configure(defines={})
+
+        # cache_entries: dict[str, str | Path] = {
+        #                 f"SKBUILD_{k.upper()}_DIR": v for k, v in wheel_dirs.items()
+        # }
+        # cache_entries["SKBUILD_STATE"] = self.target_name
+
+        builder.configure(
+            defines={},
+            # cache_entries=cache_entries,
+            # name=metadata.name,
+            # version=metadata.version
+        )
         builder.build([])
         builder.install(prefix)
 
         for path in prefix.iterdir():
             build_data["artifacts"].append(path)
-            build_data["force_include"][f"prefix/{path.relative_to(prefix)}"] = str(
-                settings.wheel.install_dir / path.relative_to(prefix)
-            )
+            # build_data["force_include"][f"prefix/{path.relative_to(prefix)}"] = str(
+            #     settings.wheel.install_dir / path.relative_to(prefix)
+            # )
 
         build_data["infer_tag"] = True
-        build_data["pure_python"] = False
+        build_data["pure_python"] = not settings.wheel.platlib
