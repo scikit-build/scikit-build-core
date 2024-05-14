845651536408ea59562da91e2d0e0646968115a4
Henry Schreiner <HenrySchreinerIII@gmail.com>
2024-05-14 13:32:30 -0400

    chore: use pylint 3.2, gha reporter (#745)
    Using this instead of our custom reporter.
    
    Signed-off-by: Henry Schreiner <henryschreineriii@gmail.com>

--- src/scikit_build_core/_version.pyi
+++ src/scikit_build_core/_version.pyi
@@ -1,4 +1,5 @@
 version: str
+# pylint: disable-next=unsupported-binary-operation
 version_tuple: tuple[int, int, int, str, str] | tuple[int, int, int]
 
 # Note that newer versions of setuptools_scm also add __version__, but we are
