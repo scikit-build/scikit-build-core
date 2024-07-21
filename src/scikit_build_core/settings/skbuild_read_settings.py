f966742d494687213e3e262a14a2ee6156c21663
Henry Schreiner <henryschreineriii@gmail.com>
2024-07-21 01:06:20 -0400

    refactor: return dicts/sets
    Signed-off-by: Henry Schreiner <henryschreineriii@gmail.com>

--- src/scikit_build_core/settings/skbuild_read_settings.py
+++ src/scikit_build_core/settings/skbuild_read_settings.py
@@ -66,7 +66,6 @@ def regex_match(value: str, match: str) -> str:
 
 def override_match(
     *,
-    match_all: bool,
     current_env: Mapping[str, str] | None,
     current_state: Literal[
         "sdist", "wheel", "editable", "metadata_wheel", "metadata_editable"
@@ -83,17 +82,14 @@ def override_match(
     state: str | None = None,
     from_sdist: bool | None = None,
     failed: bool | None = None,
-) -> set[str]:
+) -> tuple[dict[str, str], set[str]]:
     """
-    Check if the current environment matches the overrides. Returns the set of matched keys.
+    Check if the current environment matches the overrides. Returns a dict
+    of passed matches, with reasons for values, and a set of non-matches.
     """
-    # This makes a matches list. A match is a string; if it's an empty string,
-    # then it did not match. If it's not empty, it did match, and the string
-    # will be printed in the logs - it's the match reason. If match_all is True,
-    # then all must match, otherwise any match will count.
-    matches = []
 
-    passed_keys = set()
+    passed_dict = {}
+    failed_set: set[str] = set()
 
     if current_env is None:
         current_env = os.environ
@@ -101,16 +97,18 @@ def override_match(
     if python_version is not None:
         current_python_version = ".".join(str(x) for x in sys.version_info[:2])
         match_msg = version_match(current_python_version, python_version, "Python")
-        matches.append(match_msg)
         if match_msg:
-            passed_keys.add("python-version")
+            passed_dict["python-version"] = match_msg
+        else:
+            failed_set.add("python-version")
 
     if implementation_name is not None:
         current_impementation_name = sys.implementation.name
         match_msg = regex_match(current_impementation_name, implementation_name)
-        matches.append(match_msg)
         if match_msg:
-            passed_keys.add("implementation-name")
+            passed_dict["implementation-name"] = match_msg
+        else:
+            failed_set.add("implementation-name")
 
     if implementation_version is not None:
         info = sys.implementation.version
@@ -121,90 +119,84 @@ def override_match(
         match_msg = version_match(
             version, implementation_version, "Python implementation"
         )
-        matches.append(match_msg)
         if match_msg:
-            passed_keys.add("implementation-version")
+            passed_dict["implementation-version"] = match_msg
+        else:
+            failed_set.add("implementation-version")
 
     if platform_system is not None:
         current_platform_system = sys.platform
         match_msg = regex_match(current_platform_system, platform_system)
-        matches.append(match_msg)
         if match_msg:
-            passed_keys.add("platform-system")
+            passed_dict["platform-system"] = match_msg
+        else:
+            failed_set.add("platform-system")
 
     if platform_machine is not None:
         current_platform_machine = platform.machine()
         match_msg = regex_match(current_platform_machine, platform_machine)
-        matches.append(match_msg)
         if match_msg:
-            passed_keys.add("platform-machine")
+            passed_dict["platform-machine"] = match_msg
+        else:
+            failed_set.add("platform-machine")
 
     if platform_node is not None:
         current_platform_node = platform.node()
         match_msg = regex_match(current_platform_node, platform_node)
-        matches.append(match_msg)
         if match_msg:
-            passed_keys.add("platform-node")
+            passed_dict["platform-node"] = match_msg
+        else:
+            failed_set.add("platform-node")
 
     if state is not None:
         match_msg = regex_match(current_state, state)
-        matches.append(match_msg)
         if match_msg:
-            passed_keys.add("state")
+            passed_dict["state"] = match_msg
+        else:
+            failed_set.add("state")
 
     if failed is not None:
         if failed and retry:
-            matches.append("Previous run failed")
-            passed_keys.add("failed")
+            passed_dict["failed"] = "Previous run failed"
         elif not failed and not retry:
-            matches.append("Running on a fresh run")
-            passed_keys.add("failed")
+            passed_dict["failed"] = "Running on a fresh run"
         else:
-            matches.append("")
+            failed_set.add("failed")
 
     if from_sdist is not None:
         if has_dist_info:
-            matches.append("from sdist due to PKG-INFO" if from_sdist else "")
             if from_sdist:
-                passed_keys.add("from-sdist")
+                passed_dict["from-sdist"] = "from sdist due to PKG-INFO"
+            else:
+                failed_set.add("from-sdist")
+        elif not from_sdist:
+            passed_dict["from-sdist"] = "not from sdist, no PKG-INFO"
         else:
-            matches.append("" if from_sdist else "not from sdist, no PKG-INFO")
-            if not from_sdist:
-                passed_keys.add("from-sdist")
+            failed_set.add("from-sdist")
 
     if env:
         for key, value in env.items():
-            if isinstance(value, bool):
-                match_msg = (
-                    f"env {key} is {value}"
-                    if strtobool(current_env.get(key, "")) == value
-                    else ""
-                )
-                matches.append(match_msg)
-                passed_keys.add(f"env.{key}")
-            elif key not in current_env:
-                matches.append("")
+            if key not in current_env:
+                failed_set.add(f"env.{key}")
+            elif isinstance(value, bool):
+                if strtobool(current_env[key]) == value:
+                    passed_dict[f"env.{key}"] = f"env {key} is {value}"
+                else:
+                    failed_set.add(f"env.{key}")
             else:
-                current_value = current_env.get(key, "")
+                current_value = current_env[key]
                 match_msg = regex_match(current_value, value)
-                matches.append(match_msg and f"env {key}: {match_msg}")
-                if match_msg:
-                    passed_keys.add(f"env.{key}")
 
-    if not matches:
+                if match_msg:
+                    passed_dict[f"env.{key}"] = f"env {key}: {match_msg}"
+                else:
+                    failed_set.add(f"env.{key}")
+
+    if not passed_dict and not failed_set:
         msg = "At least one override must be provided"
         raise ValueError(msg)
 
-    if match_all:
-        matched = all(matches)
-        if matched:
-            logger.info("Overrides {}", " and ".join(matches))
-    else:
-        matched = any(matches)
-        if matched:
-            logger.info("Overrides {}", " or ".join([m for m in matches if m]))
-
-    return passed_keys if matched else set()
+    return passed_dict, failed_set
 
 
 def _handle_minimum_version(
@@ -326,7 +318,9 @@ def process_overides(
 
     global_matched: set[str] = set()
     for override in tool_skb.pop("overrides", []):
-        matched: set[str] | None = None
+        passed_any: dict[str, str] | None = None
+        passed_all: dict[str, str] | None = None
+        failed: set[str] = set()
         if_override = override.pop("if", None)
         if not if_override:
             msg = "At least one 'if' override must be provided"
@@ -337,8 +331,7 @@ def process_overides(
         if "any" in if_override:
             any_override = if_override.pop("any")
             select = {k.replace("-", "_"): v for k, v in any_override.items()}
-            matched = override_match(
-                match_all=False,
+            passed_any, _ = override_match(
                 current_env=env,
                 current_state=state,
                 has_dist_info=has_dist_info,
@@ -353,24 +346,37 @@ def process_overides(
 
         select = {k.replace("-", "_"): v for k, v in if_override.items()}
         if select:
-            each_matched = override_match(
-                match_all=True,
+            passed_all, failed = override_match(
                 current_env=env,
                 current_state=state,
                 has_dist_info=has_dist_info,
                 retry=retry,
                 **select,
             )
-            if matched is None:
-                matched = each_matched
-            elif matched and each_matched:
-                matched |= each_matched
-            else:
-                matched = set()
-        if matched is None:
-            matched = set()
-        global_matched |= matched
-        if matched:
+
+        # If no overrides are passed, do nothing
+        if passed_any is None and passed_all is None:
+            continue
+
+        # If normal overrides are passed and one or more fails, do nothing
+        if passed_all is not None and failed:
+            continue
+
+        # If any is passed, at least one always needs to pass.
+        if passed_any is not None and not passed_any:
+            continue
+
+        local_matched = set(passed_any or []) | set(passed_all or [])
+        global_matched |= local_matched
+        if local_matched:
+            all_str = " and ".join(
+                [
+                    *(passed_all or {}).values(),
+                    *([" or ".join(passed_any.values())] if passed_any else []),
+                ]
+            )
+            logger.info("Overrides {}", all_str)
+
             for key, value in override.items():
                 inherit1 = inherit_override.get(key, {})
                 if isinstance(value, dict):
