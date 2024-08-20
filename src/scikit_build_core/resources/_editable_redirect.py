4cfafe2b8419b02ec92fed69c70396d413688721
Henry Schreiner <HenrySchreinerIII@gmail.com>
2024-08-20 11:34:14 -0400

    fix: path will always be relative

--- src/scikit_build_core/resources/_editable_redirect.py
+++ src/scikit_build_core/resources/_editable_redirect.py
@@ -43,11 +43,8 @@ class ScikitBuildRedirectingFinder(importlib.abc.MetaPathFinder):
         self.build_options = build_options
         self.install_options = install_options
         self.dir = dir
-        self.install_dir = (
-            install_dir
-            if os.path.isabs(install_dir)
-            else os.path.join(DIR, install_dir)
-        )
+        self.install_dir = os.path.join(DIR, install_dir)
+
         # Construct the __path__ of all resource files
         # I.e. the paths of all package-like objects
         submodule_search_locations: dict[str, set[str]] = {}
