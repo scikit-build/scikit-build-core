7081389b71c779c7a931d4d16ad1a05fd01ef3d8
Vyas Ramasubramani <vyasr@nvidia.com>
2024-10-18 16:02:21 -0700

    Add more nested imports

--- tests/packages/importlib_editable/pkg/__init__.py
+++ tests/packages/importlib_editable/pkg/__init__.py
@@ -1,3 +1,48 @@
-from . import emod_a, pmod_a, subpkg1, subpkg2
+# Don't let ruff sort imports in this file, we want to keep them with the comments as is
+# for clarity.
+# ruff: noqa: I001
 
-__all__ = ["emod_a", "pmod_a", "subpkg1", "subpkg2"]
+# Level one pure modules
+from . import pmod_a
+
+# Level one extension modules
+from . import emod_a
+
+# Level one subpackages
+from . import subpkg1, subpkg2
+
+# Level two pure modules
+from .subpkg1 import pmod_b
+from .subpkg2 import pmod_c
+
+# Level two extension modules
+from .subpkg1 import emod_b
+from .subpkg2 import emod_c
+
+# Level two subpackages
+from .subpkg2 import subsubpkg1, subsubpkg2
+
+# Level three pure modules
+from .subpkg2.subsubpkg1 import pmod_d
+from .subpkg2.subsubpkg2 import pmod_e
+
+# Level three extension modules
+from .subpkg2.subsubpkg1 import emod_d
+from .subpkg2.subsubpkg2 import emod_e
+
+__all__ = [
+    "emod_a",
+    "emod_b",
+    "emod_c",
+    "emod_d",
+    "emod_e",
+    "pmod_a",
+    "pmod_b",
+    "pmod_c",
+    "pmod_d",
+    "pmod_e",
+    "subpkg1",
+    "subpkg2",
+    "subsubpkg1",
+    "subsubpkg2",
+]
--- tests/packages/importlib_editable/pkg/subpkg2/__init__.py
+++ tests/packages/importlib_editable/pkg/subpkg2/__init__.py
@@ -1,3 +1,31 @@
-from . import emod_c, pmod_c, subsubpkg1, subsubpkg2
+# Don't let ruff sort imports in this file, we want to keep them with the comments as is
+# for clarity.
+# ruff: noqa: I001
 
-__all__ = ["emod_c", "pmod_c", "subsubpkg1", "subsubpkg2"]
+# Level one pure modules
+from . import pmod_c
+
+# Level one extension modules
+from . import emod_c
+
+# Level one subpackages
+from . import subsubpkg1, subsubpkg2
+
+# Level two pure modules
+from .subsubpkg1 import pmod_d
+from .subsubpkg2 import pmod_e
+
+# Level two extension modules
+from .subsubpkg1 import emod_d
+from .subsubpkg2 import emod_e
+
+__all__ = [
+    "emod_c",
+    "emod_d",
+    "emod_e",
+    "pmod_c",
+    "pmod_d",
+    "pmod_e",
+    "subsubpkg1",
+    "subsubpkg2",
+]
