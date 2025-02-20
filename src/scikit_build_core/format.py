0c4e7f92e15d81a5aa668daa7507dc50068c4022
Cristian Le <git@lecris.dev>
2025-02-20 14:52:42 +0100

    Implement the `build_dir` format variables
    Signed-off-by: Cristian Le <git@lecris.dev>

--- src/scikit_build_core/format.py
+++ src/scikit_build_core/format.py
@@ -2,9 +2,13 @@
 
 from __future__ import annotations
 
+import sys
 from typing import TYPE_CHECKING, TypedDict
 
 if TYPE_CHECKING:
+    from typing import Literal
+
+    from scikit_build_core.builder.wheel_tag import WheelTag
     from scikit_build_core.settings.skbuild_model import ScikitBuildSettings
 
 __all__ = [
@@ -24,14 +28,36 @@ class PyprojectFormatter(TypedDict, total=False):
     in the pyproject.toml config file.
     """
 
+    cache_tag: str
+    wheel_tag: str
+    build_type: str
+    state: str
+
 
 def pyproject_format(
     *,
-    settings: ScikitBuildSettings | None = None,  # noqa: ARG001
+    settings: ScikitBuildSettings | None = None,
+    state: (
+        Literal["sdist", "wheel", "editable", "metadata_wheel", "metadata_editable"]
+        | None
+    ) = None,
+    tags: WheelTag | None = None,
     dummy: bool = False,
 ) -> PyprojectFormatter | dict[str, str]:
     """Generate :py:class:`PyprojectFormatter` dictionary to use in f-string format."""
     if dummy:
         # Return a dict with all the known keys but with values replaced with dummy values
         return {key: "*" for key in PyprojectFormatter.__annotations__}
-    return PyprojectFormatter()
+    # First set all known values
+    res = PyprojectFormatter(
+        cache_tag=sys.implementation.cache_tag,
+    )
+    # Then compute all optional keys depending on the function input
+    if settings is not None:
+        res["build_type"] = settings.cmake.build_type
+    if tags is not None:
+        res["wheel_tag"] = str(tags)
+    if state is not None:
+        res["state"] = state
+    # Construct the final dict including the always known keys
+    return res
