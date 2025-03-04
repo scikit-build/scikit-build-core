275d3672bfec05c22c0a4d48d3a4edf6137fc7b3
Cristian Le <git@lecris.dev>
2025-03-04 15:42:27 +0100

    Add skeleton for WheelRepairer
    Signed-off-by: Cristian Le <git@lecris.dev>

---
+++ src/scikit_build_core/repair_wheel/linux.py
@@ -0,0 +1,32 @@
+"""
+Repair Linux RPATH
+"""
+
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+import auditwheel  # noqa: F401
+
+from . import WheelRepairer
+
+if TYPE_CHECKING:
+    from ..file_api.model.codemodel import Target
+
+__all__ = ["LinuxWheelRepairer"]
+
+
+def __dir__() -> list[str]:
+    return __all__
+
+
+class LinuxWheelRepairer(WheelRepairer):
+    """
+    Adjust the RPATH with $ORIGIN.
+    """
+
+    _platform = "Linux"
+
+    def patch_target(self, target: Target) -> None:
+        # TODO: Implement patching
+        pass
