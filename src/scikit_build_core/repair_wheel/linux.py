7d6c17ff4ecc3f8460d0d55b7eccfd7b0a795341
Cristian Le <git@lecris.dev>
2025-03-12 16:20:35 +0100

    Run pachelf directly
    `auditwheel` pacher forces `DT_RPATH`. Here instead we use `DT_RUNPATH` so that it can be overwritten by the user
    
    Signed-off-by: Cristian Le <git@lecris.dev>

--- src/scikit_build_core/repair_wheel/linux.py
+++ src/scikit_build_core/repair_wheel/linux.py
@@ -6,6 +6,7 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING
 
+from .._shutil import Run
 from .rpath import RpathWheelRepairer
 
 if TYPE_CHECKING:
@@ -36,9 +37,8 @@ class LinuxWheelRepairer(RpathWheelRepairer):
         ]
 
     def patch_library_rpath(self, artifact: Path, rpaths: list[str]) -> None:
-        from auditwheel.patcher import Patchelf
-
         final_rpaths = set(rpaths)
         if final_rpaths:
-            patcher = Patchelf()
-            patcher.set_rpath(artifact, ":".join(final_rpaths))
+            run = Run()
+            run.live("patchelf", "--remove-rpath", artifact)
+            run.live("patchelf", "--set-rpath", ":".join(final_rpaths), artifact)
