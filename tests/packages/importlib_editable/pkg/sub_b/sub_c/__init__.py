e39cf5a569dbd5e6a01e00432eb084a8298fed18
Cristian Le <git@lecris.dev>
2025-03-19 18:55:25 +0100

    Add cousin navigation
    Signed-off-by: Cristian Le <git@lecris.dev>

--- tests/packages/importlib_editable/pkg/sub_b/sub_c/__init__.py
+++ tests/packages/importlib_editable/pkg/sub_b/sub_c/__init__.py
@@ -3,3 +3,8 @@
 
 from .pmod_d import square as psquare_d
 from .emod_d import square as esquare_d
+
+# Level one import cousin
+from .. import sub_d
+from ..sub_d.pmod_e import square as psquare_e
+from ..sub_d.emod_e import square as esquare_e
