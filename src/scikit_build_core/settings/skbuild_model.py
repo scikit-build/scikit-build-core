cd0602b1de45066fce1bc1f6553acaa1338f7573
Cristian Le <git@lecris.dev>
2025-09-19 18:34:56 +0200

    Add `cmake.cache-file` option

--- src/scikit_build_core/settings/skbuild_model.py
+++ src/scikit_build_core/settings/skbuild_model.py
@@ -133,6 +133,11 @@ class CMakeSettings:
     DEPRECATED in 0.10; use build.targets instead.
     """
 
+    cache_file: Optional[Path] = None
+    """
+    CMake cache file to be loaded.
+    """
+
 
 @dataclasses.dataclass
 class SearchSettings:
