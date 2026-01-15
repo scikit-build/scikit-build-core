a4c356422bf46b4f8b97d7d6ee63b7ef208125a4
M. Eric Irrgang <vardarirrgan1@llnl.gov>
2026-01-15 10:51:32 -0800

    `files()` requires an argument before Python 3.12.

--- tests/packages/cmake_generated/src/cmake_generated/nested2/__init__.py
+++ tests/packages/cmake_generated/src/cmake_generated/nested2/__init__.py
@@ -1,5 +1,5 @@
 def nested1_generated_check():
     # noinspection PyUnresolvedReferences
-    from ..nested1.generated import nested_data
+    from ..nested1.generated import nested_data  # type: ignore[import-not-found]
 
     return nested_data
