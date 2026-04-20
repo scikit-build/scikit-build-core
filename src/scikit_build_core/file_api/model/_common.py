1df8c60a46a0132c8bcaf6c77c344d13a3f9a80f
Cristian Le <git@lecris.dev>
2026-04-20 13:34:22 +0200

    Make APIVersion oderable
    Signed-off-by: Cristian Le <git@lecris.dev>

--- src/scikit_build_core/file_api/model/_common.py
+++ src/scikit_build_core/file_api/model/_common.py
@@ -1,4 +1,5 @@
 import dataclasses
+from functools import total_ordering
 from pathlib import Path
 from typing import List
 
@@ -9,10 +10,24 @@ def __dir__() -> List[str]:
     return __all__
 
 
-@dataclasses.dataclass(frozen=True)
+@total_ordering
+@dataclasses.dataclass(frozen=True, eq=True)
 class APIVersion:
     major: int
     minor: int
+
+    def __gt__(self, other: "APIVersion") -> bool:
+        if not isinstance(other, APIVersion):
+            raise NotImplementedError
+        if self.major > other.major:
+            return True
+        if self.major < other.major:
+            return False
+        if self.minor > other.minor:
+            return True
+        if self.minor < other.minor:
+            return False
+        return False
 
 
 @dataclasses.dataclass(frozen=True)
