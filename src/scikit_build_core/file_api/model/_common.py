adcf0ab44d87cb6ea03d4e11b2efb416ecbb9ddf
Cristian Le <git@lecris.dev>
2026-04-20 13:46:40 +0200

    Add a base ObjectKind type
    Signed-off-by: Cristian Le <git@lecris.dev>

--- src/scikit_build_core/file_api/model/_common.py
+++ src/scikit_build_core/file_api/model/_common.py
@@ -1,9 +1,12 @@
 import dataclasses
+from abc import ABC
 from functools import total_ordering
 from pathlib import Path
-from typing import List
+from typing import ClassVar, List, Union
 
-__all__ = ["APIVersion", "Paths"]
+from ..._logging import logger
+
+__all__ = ["APIVersion", "ObjectKind", "Paths"]
 
 
 def __dir__() -> List[str]:
@@ -29,8 +32,43 @@ class APIVersion:
             return False
         return False
 
+    def __str__(self) -> str:
+        return f"{self.major}.{self.minor}"
+
 
 @dataclasses.dataclass(frozen=True)
 class Paths:
     source: Path
     build: Path
+
+
+class ObjectKind(ABC):
+    kind: ClassVar[str]
+    version: ClassVar[APIVersion]
+    _objectkind_registrar: ClassVar[dict[str, set[type["ObjectKind"]]]] = {}
+
+    def __init_subclass__(cls) -> None:
+        registrar = cls._objectkind_registrar.setdefault(cls.kind, set())
+        registrar.add(cls)
+
+    @classmethod
+    def get_object_kind(
+        cls, kind: str, version: Union[APIVersion, dict[str, int]]
+    ) -> type["ObjectKind"]:
+        if isinstance(version, dict):
+            return cls.get_object_kind(
+                kind=kind,
+                version=APIVersion(major=version["major"], minor=version["minor"]),
+            )
+        registrar = cls._objectkind_registrar[kind]
+        for object_kind in registrar:
+            if version == object_kind.version:
+                return object_kind
+        assert version > object_kind.version
+        logger.warning(
+            "The file-api object kind '{} {}' has not been registered in scikit-build-core",
+            kind,
+            version,
+        )
+        logger.debug("Using best guess {}", object_kind.__name__)
+        return object_kind
